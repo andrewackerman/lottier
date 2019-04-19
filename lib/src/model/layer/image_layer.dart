903ecf9c02208c5df2a2eb0d555e0b7eb25e413d
Andrew Ackerman <andrew@canaryspeech.com>
2019-04-19 17:33:47 -0600

    Committing progress

--- lib/src/model/layer/image_layer.dart
+++ lib/src/model/layer/image_layer.dart
@@ -6,6 +6,7 @@ import './layer_base.dart';
 import '../../lottier_widget.dart';
 import './layer.dart';
 import '../../utility/utility.dart';
+import '../../animation/keyframe/keyframe_animation_base.dart';
 
 class ImageLayer extends LayerBase {
 
@@ -18,6 +19,8 @@ class ImageLayer extends LayerBase {
     Rect _src = Rect.zero;
     Rect _dst = Rect.zero;
 
+    KeyframeAnimationBase<ColorFilter, ColorFilter> _colorFilterAnimation;
+
     @override
     void drawLayer(Canvas canvas, Matrix3 parentMatrix, int parentAlpha) {
       final image = _getImage();
@@ -29,10 +32,35 @@ class ImageLayer extends LayerBase {
       final density = Utility.dpScale;
 
       _paint.color = _paint.color.withAlpha(parentAlpha);
-      if (colorFilterAnimation != null)
+      if (_colorFilterAnimation != null) {
+        _paint.colorFilter = _colorFilterAnimation.value;
+      }
+
+      canvas.save();
+
+      List<double> nums = List(9);
+      matrix.copyIntoArray(nums);
+      canvas.transform(nums);
+
+      _src = Rect.fromLTRB(0, 0, image.width.toDouble(), image.height.toDouble());
+      _dst = Rect.fromLTRB(0, 0, image.width * density, image.height * density);
 
       canvas.drawImageRect(image, _src, _dst, _paint);
       canvas.restore();
+    }
+
+    @override
+    Rect getBounds(Rect bounds, Matrix3 parentMatrix, bool applyParents) {
+      bounds = super.getBounds(bounds, parentMatrix, applyParents);
+
+      Image image = _getImage();
+      if (image != null) {
+        final density = Utility.dpScale;
+        bounds = Rect.fromLTRB(0, 0, image.width * density, image.height * density);
+        bounds = Utility.applyMatrixToRect(boundsMatrix, bounds);
+      }
+
+      return bounds;
     }
 
     Image _getImage() {
